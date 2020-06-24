dbb232287c4e161cbbb2b8cf540b27bdf6de7b88
Carolina Almeida <carolina.almeida@fabricadeaplicativos.com.br>
2020-06-24 11:15:34 -0300

    create draggable card widget

---
+++ lib/widgets/draggable_card.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class DraggableCard extends StatefulWidget {
+  const DraggableCard({this.color});
+  final Color color;
+
+  @override
+  DraggableCardState createState() {
+    return new DraggableCardState();
+  }
+}
+
+class DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Alignment _dragAlignment = Alignment.center;
+  Animation<Alignment> _animation;
+  bool _selected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+
+    _controller.addListener(() {
+      setState(() {
+        _selected = _selected;
+        _dragAlignment = _animation.value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onPanDown: (details) {
+        _controller.stop();
+        setState(() {
+          _selected = true;
+        });
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _selected = true;
+          _dragAlignment += Alignment(
+            details.delta.dx / (size.width / 2),
+            details.delta.dy / (size.height / 2),
+          );
+        });
+      },
+      onPanEnd: (details) {
+        setState(() {
+          _selected = false;
+        });
+      },
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(
+            color: widget.color,
+            shape: StadiumBorder(
+              side: BorderSide(
+                color:
+                    _selected ? darken(widget.color, 40) : Colors.transparent,
+                width: 2.0,
+              ),
+            ),
+            elevation: _selected ? 5 : 0,
+            child: Container(
+              width: 30,
+              height: 30,
+            )),
+      ),
+    );
+  }
+
+  Color darken(Color c, [int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+    var f = 1 - percent / 100;
+    return Color.fromARGB(c.alpha, (c.red * f).round(), (c.green * f).round(),
+        (c.blue * f).round());
+  }
+}
